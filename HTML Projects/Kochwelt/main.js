8d3bf356f60a89becc8b83356925c859c7521eab
fepan0283 <96198087+fepan0283@users.noreply.github.com>
2022-01-13 18:31:32 +0100

    commit

--- HTML Projects/Kochwelt/main.js
+++ HTML Projects/Kochwelt/main.js
@@ -25,78 +25,96 @@ function includeHTML() {
     }
   }
 }
+let recipeList = [
+  {
+    "name": "Mini-Croissants",
+    "image": "03-Gruppenarbeit-Kochwelt/img/croissant.jpg",
+    "description": "Der leckere Snack für zwischendurch",
+    "creator": "Felix Pan",
+    "ranking": 4,
+    "prep-time": 15,
+    "difficulty": "einfach",
+    "create-date": "13.01.2022",
+    "ingredients": ["Pfannkuchen", 1, "Eier", 100, "g Mehl", 50, "g Zucker", 125, "g cream"]
+  },
+  {
+    "name": "deftige Knödel",
+    "image": "03 - Gruppenarbeit - Kochwelt/img/croissant.jpg",
+    "description": "ein ganz schön schwere Mittagessen",
+    "creator": "Michael Pan",
+    "ranking": 3,
+    "prep-time": 35,
+    "difficulty": "mittel",
+    "ingredients": [10, "Eier", 100, "g Mehl", 50, "g Zucker", 125, "g cream"]
+  },
+  {
+    "name": "Pfannkuchen",
+    "image": "03 - Gruppenarbeit - Kochwelt/img/croissant.jpg",
+    "description": "für ein schnelles Mittagessen für die ganze Familie",
+    "creator": "Nicoline Pan",
+    "ranking": 5,
+    "prep-time": 25,
+    "difficulty": "einfach",
+    "ingredients": [100, "Eier", 100, "g Mehl", 50, "g Zucker", 125, "g cream"]
+  }
+]
 
-let recipe = ["Pfannkuchen", 1, "Eier", 100, "g Mehl", 50, "g Zucker", 125, "g cream"];
+let recipe = [10, "Eier", 100, "g Mehl", 50, "g Zucker", 125, "g cream"];
 
-function loadRecipe() {
-  for (let i = 1; i < recipe.length; i += 2) {
-    document.getElementById('ingredients').innerHTML += `<p class="ingredient">${recipe[i]} ${recipe[i + 1]}</p>`;
+function loadRecipe(recipechoice) {
+  for (let i = 1; i < recipeList[recipechoice]["ingredients"].length; i += 2) {
+    document.getElementById('ingredients').innerHTML += `<p class="ingredient">${recipeList[recipechoice]["ingredients"][i]} ${recipeList[recipechoice]["ingredients"][i + 1]}</p>`;
   }
 }
 
-function calcingredients() {
+function calcingredients(recipechoice) {
   let numpeople = +document.getElementById('numpeople').value;
   document.getElementById('ingredients').innerHTML = ``;
-  for (let i = 1; i < recipe.length; i += 2) {
-    document.getElementById('ingredients').innerHTML += `<p class="ingredient">${recipe[i] * numpeople} ${recipe[i + 1]}</p>`;
+  for (let i = 1; i < recipeList[recipechoice]["ingredients"].length; i += 2) {
+    document.getElementById('ingredients').innerHTML += `<p class="ingredient">${recipeList[recipechoice]["ingredients"][i] * numpeople} ${recipeList[recipechoice]["ingredients"][i + 1]}</p>`;
   }
 }
 
-// import { recipe1 } from 'recipedata.js';
-
-let recipe1 = {
-  "name": "Mini-Croissants",
-  "image": "03 - Gruppenarbeit - Kochwelt/img/croissant.jpg",
-  "description": "Der leckere Snack für zwischendurch",
-  "creator": "Felix Pan",
-  "ranking": 4,
-  "prep-time": 15,
-  "difficulty": "einfach"
-}
-let recipe2 = {
-  "name": "Mini-Croissants",
-  "image": "03 - Gruppenarbeit - Kochwelt/img/croissant.jpg",
-  "description": "Der leckere Snack für zwischendurch",
-  "creator": "Felix Pan",
-  "ranking": 4,
-  "prep-time": 15,
-  "difficulty": "einfach"
-}
-
-let recipeList = [recipe1, recipe2];
 
 function loadallrecipes() {
   for (i = 0; i < recipeList.length; i++) {
 
     document.getElementById('recipe-container').innerHTML += `
 
-    <div class="recipe">
-    <a href="#"><img src=${recipe1["image"]}></a>
-    <a href="#">
-        <h5>${recipe1["name"]}</h5>
-    </a>
-    <p>${recipe1["description"]}</p>
-    <p style="font-weight: bold;">von ${recipe1["creator"]}</p>
+    <div id="recipe" class="recipe">
+    <a href="#" onclick="openrecipechoice(2)"><img src=${recipeList[i]["image"]}></a>
+    <a href="#" onclick="openrecipechoice(2)"><h5>${recipeList[i]["name"]}</h5></a>
+    <p>${recipeList[i]["description"]}</p>
+    <p style="font-weight: bold;">von ${recipeList[i]["creator"]}</p>
 
-    <div class="reviewstars">
-        <img src="03 - Gruppenarbeit - Kochwelt/img/icons/star-2-48.png" alt="">
-        <img src="03 - Gruppenarbeit - Kochwelt/img/icons/star-2-48.png" alt="">
-        <img src="03 - Gruppenarbeit - Kochwelt/img/icons/star-2-48.png" alt="">
-        <img src="03 - Gruppenarbeit - Kochwelt/img/icons/star-2-48.png" alt="">
+    <div id="ranking${i}" class="reviewstars">
     </div>
     <div class="recipe-icons">
         <div class="tag">
             <img src="03 - Gruppenarbeit - Kochwelt/img/icons/clock-regular.svg" alt="">
-            <p>${recipe1["prep-time"]} Min</p>
+            <p>${recipeList[i]["prep-time"]} Min</p>
         </div>
         <div class="tag">
             <img src="03 - Gruppenarbeit - Kochwelt/img/icons/brain-solid.svg" alt="">
-            <p>${recipe1["difficulty"]}</p>
+            <p>${recipeList[i]["difficulty"]}</p>
         </div>
     </div>
   </div>
 
   `;
   }
+  updatestars()
+}
 
+function updatestars() {
+  for (j = 0; j < recipeList.length; j++) {
+    for (i = 0; i < recipeList[j]["ranking"]; i++) {
+      document.getElementById(`ranking${j}`).innerHTML += `<img src="03 - Gruppenarbeit - Kochwelt/img/icons/star-2-48.png" alt="">`;
+    }
+  }
+}
+
+function openrecipechoice(recipechoice) {
+  open("recipechoice.html")
+  document.getElementById('recipe-container-test').innerHTML = `<p>${recipeList[recipechoice]["ingredients"][0]}</p>`;
 }
