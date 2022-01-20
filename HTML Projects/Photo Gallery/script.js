f9ef291b0ef09821195e5ca4ec2a78e9bcb6d7db
fepan0283 <96198087+fepan0283@users.noreply.github.com>
2022-01-20 13:15:04 +0100

    commit

--- HTML Projects/Photo Gallery/script.js
+++ HTML Projects/Photo Gallery/script.js
@@ -8,15 +8,29 @@ let pictures = [
     "/gallerypics/7.jpg",
     "/gallerypics/8.jpg",
     "/gallerypics/9.jpg",
+    "/gallerypics/1.jpg",
 ];
 
 function loadAllImages() {
     for (i = 0; i < pictures.length; i++) {
         document.getElementById("picture-container").innerHTML += `
-        <img src="${pictures[i]}">
-        
-        
-        
+        <div onclick="zoomInPicture(this.id)" id="picture${i}"  class="image-container">
+            <img src="${pictures[i]}">
+            <div class="image-hover" </div>
+        </div>
         `;
     }
 }
+
+function zoomInPicture(id) {
+    let pictureId = id.slice(-1);
+    console.log(id);
+    document.getElementById('zoomed-picture').src = pictures[pictureId];
+    document.getElementById('zoom-picture-container').classList.remove("d-none");
+}
+
+function closeZoomPicture() {
+    document.getElementById('zoom-picture-container').classList.add("d-none");
+}
+
+// function mit rechts und links Pfeilen
